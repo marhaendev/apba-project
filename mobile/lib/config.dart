98adcaae09be2a70d5aabd35ea79894b1619b6fa
marhaendev <marhaendev@gmail.com>
2026-01-23 13:16:11 +0700

    feat: integrate dotenv in mobile app

--- mobile/lib/config.dart
+++ mobile/lib/config.dart
@@ -1,3 +1,6 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class AppConfig {
-  static const String baseUrl = 'http://192.168.1.12:3001/api';
+  static String get baseUrl =>
+      dotenv.get('MOBILE_API_URL', fallback: 'http://192.168.1.12:3001/api');
 }
--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'login.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: "../.env");
   runApp(MyApp());
 }
 
